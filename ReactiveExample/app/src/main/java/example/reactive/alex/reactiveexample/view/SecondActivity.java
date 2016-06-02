80c7b47b87d3e34acb8430f3fd0a63b880b0084c
AlexKucherenko <alk@qapint.com>
2016-06-02 14:58:43 +0300

    Fix PLUS action

--- ReactiveExample/app/src/main/java/example/reactive/alex/reactiveexample/view/SecondActivity.java
+++ ReactiveExample/app/src/main/java/example/reactive/alex/reactiveexample/view/SecondActivity.java
@@ -3,7 +3,10 @@ package example.reactive.alex.reactiveexample.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.AlteredCharSequence;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -18,6 +21,7 @@ import static trikita.anvil.BaseDSL.MATCH;
 import static trikita.anvil.BaseDSL.dip;
 import static trikita.anvil.BaseDSL.margin;
 import static trikita.anvil.BaseDSL.o;
+import static trikita.anvil.BaseDSL.onTextChanged;
 import static trikita.anvil.BaseDSL.padding;
 import static trikita.anvil.BaseDSL.size;
 import static trikita.anvil.BaseDSL.text;
@@ -28,13 +32,14 @@ import static trikita.anvil.DSL.inputType;
 import static trikita.anvil.DSL.linearLayout;
 import static trikita.anvil.DSL.onClick;
 import static trikita.anvil.DSL.orientation;
+import static trikita.anvil.DSL.rawInputType;
 
 /**
  * Created by Alex on 01.06.2016.
  */
 public class SecondActivity extends AppCompatActivity {
 
-    private CharSequence editTextStringBuilder = "0";
+    private CharSequence editTextString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +54,9 @@ public class SecondActivity extends AppCompatActivity {
                         orientation(LinearLayout.VERTICAL),
 
                         o(editText(),
-                                text(editTextStringBuilder),
-                                inputType(InputType.TYPE_CLASS_NUMBER),
+                                text(editTextString),
+                                rawInputType(InputType.TYPE_CLASS_NUMBER),
+                                onTextChanged(ontextChangeListener),
                                 hint("enter number to add")),
 
                         o(button(),
@@ -64,7 +70,26 @@ public class SecondActivity extends AppCompatActivity {
     private final View.OnClickListener plusListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            App.dispatch(new Action<>(ActionType.PLUS, Integer.parseInt(editTextStringBuilder.toString())));
+            if (!TextUtils.isEmpty(editTextString)) {
+                App.dispatch(new Action<>(ActionType.PLUS, Integer.parseInt(editTextString.toString())));
+            }
+        }
+    };
+
+    private final TextWatcher ontextChangeListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            editTextString = s;
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
         }
     };
 }
